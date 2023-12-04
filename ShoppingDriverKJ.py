949327694d76e4c3b938bb02eb2a8d2614ad1c69
Kevin Jonathan <142511694+KevinJ0nathan@users.noreply.github.com>
2023-12-04 15:28:20 +0700

    Update ShoppingDriverKJ.py

--- ShoppingDriverKJ.py
+++ ShoppingDriverKJ.py
@@ -1,7 +1,7 @@
 from ShoppingKJ import Shopping
 
-# A function that creates a list of objects
-def lob():
+# A function that creates a list of objects (lob = list of objects)
+def lobKJ():
     # Makes an Empty List
     foodList = list()
     # Validation for number of items
@@ -15,16 +15,27 @@ def lob():
         else:
             print("Number of items must be at least 1.")
             continue
+    # Loop through the range of items specified by numOfItems
     for i in range(numOfItems):
         print(f'Item #{i+1}-')
+        # Prompt the user to enter the type of food
         food = input("Enter Food:")
-        foodAmount = float(input("Enter amount of pounds:"))
+        # Prompt the user to enter the amount of food in pounds and validation
+        while True:
+            foodAmount = float(input("Enter amount of pounds:"))
+            if foodAmount > 0:
+                break
+            else:
+                print("Amount of pounds must be greater than 0.")
+                continue
+        # Create a Shopping object with the entered food and its amount
         foodobj = Shopping(food,foodAmount)
+        # Append the Shopping object to the foodList
         foodList.append(foodobj)
     return foodList
 
 # A function to display the contents of the list.
-def displayList(foodList):
+def displayListKJ(foodList):
     print("")
     print("Here's a summary of the items purchased:")
     print("-----------------------------------------")
@@ -35,16 +46,20 @@ def displayList(foodList):
         print(f'Price of order: ${food.totalPrice}\n')
 
 # A function that calculates the total cost of all items. 
-def totalCost(foodList):
+def totalCostKJ(foodList):
+    # Creates an empty list
     foodPrices = list()
+    #Loop through all the items in the list of objects
     for food in foodList:
+        # Access the total price of the specified food and put it in the empty list
         foodPrices.append(food.totalPrice)
+    # Returns the total sum of price
     return sum(foodPrices)
 
 # A main function
-def fancyShopping():
-    foodlist = lob()
-    displayList(foodlist)
-    print(f'Total cost: ${totalCost(foodlist)}')
+def fancyShoppingKJ():
+    foodlist = lobKJ()
+    displayListKJ(foodlist)
+    print(f'Total cost: ${totalCostKJ(foodlist)}')
 
-fancyShopping()
+fancyShoppingKJ()
